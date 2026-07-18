da0b774756051c8950ce0f6775c10650f41aa858
mrmarkuz <mrmarkuz666@gmail.com>
2026-07-18 23:54:43 +0200

    Add cert mgmt

--- build-images.sh
+++ build-images.sh
@@ -43,6 +43,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
+	--label="org.nethserver.min-core=3.12.4-0" \
     --label="org.nethserver.images=docker.io/louislam/uptime-kuma:2.2.1" \
     "${container}"
 # Commit the image
